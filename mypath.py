b06868d575885897ed160ff6d5e07a688ca5609e
jfzhang95 <jessezhjf@gmail.com>
2018-07-31 11:20:38 +0800

    First commit

--- mypath.py
+++ mypath.py
@@ -2,11 +2,20 @@ class Path(object):
     @staticmethod
     def db_dir(database):
         if database == 'ucf101':
+            # folder that contains class labels
+            root_dir = '/path/to/UCF-101'
+
+            # Save preprocess data into output_dir
             output_dir = '/path/to/VAR/ucf101'
-            return '/path/to/UCF-101', output_dir  # folder that contains class labels
+
+            return root_dir, output_dir
         elif database == 'hmdb51':
+            # folder that contains class labels
+            root_dir = '/path/to/hmdb-51'
+
             output_dir = '/path/to/VAR/hmdb51'
-            return '/path/to/hmdb-51', output_dir # folder that contains class labels
+
+            return root_dir, output_dir
         else:
             print('Database {} not available.'.format(database))
             raise NotImplementedError
